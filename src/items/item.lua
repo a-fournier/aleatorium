f7cc1d6da731220551d252b7b8790f8fcc156766
afournier <afournier@solocal.com>
2026-01-01 11:21:18 +0100

    fix: save items pools on current game

--- src/items/item.lua
+++ src/items/item.lua
@@ -1,6 +1,7 @@
 local ItemManager = require("src/items/item_manager")
 local Logger = require("src/utils/logger")
 local Pools = require("src/pools/pools")
+local SaveManager = require("src/save/save_manager")
 
 local Item = {}
 Item.__index = Item
@@ -8,7 +9,7 @@ Item.__index = Item
 function Item:new(id, pools, isDefaultUnlock, unlockSprite)
     local o = setmetatable({}, self)
     o.id = id
-    o.pools = pools
+    o.pools = SaveManager.current_game.items[tostring(self.id)].pools or pools
     o.unlockSprite = unlockSprite
     o.isUnlocked = isDefaultUnlock
     if not isDefaultUnlock then
@@ -23,6 +24,8 @@ function Item:decrementWeight(pool)
     for p, value in pairs(self.pools) do
         self.pools[p] = math.max(0, value - decrementRatio)
     end
+
+    SaveManager.current_game.items[tostring(self.id)] = { pools = self.pools }
 end
 
 return Item
