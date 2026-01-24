33a248fa4a1bf7427d8978edcbe5d9dae7448601
afournier <afournier@solocal.com>
2026-01-24 16:17:57 +0100

    fix: debug player collision

--- src/debug/debug_manager.lua
+++ src/debug/debug_manager.lua
@@ -11,16 +11,23 @@ local function OnPickupCollision(_, entity, _)
     Logger.debug("Pickup collided:", "Type =", entity.Type, "Variant =", entity.Variant, "SubType =", entity.SubType)
 end
 
+local function OnPlayerCollision(_, entity, collided)
+    Logger.debug("Player collided:", "Type =", collided.Type, "Variant =", collided.Variant, "SubType =", collided.SubType)
+end
+
 function DebugManager.register(mod)
     MOD_REF = mod
     mod:AddCallback(ModCallbacks.MC_POST_ENTITY_KILL, OnKill)
     mod:AddCallback(ModCallbacks.MC_PRE_PICKUP_COLLISION, OnPickupCollision)
+    mod:AddCallback(ModCallbacks.MC_PRE_PLAYER_COLLISION, OnPlayerCollision)
+
 end
 
 function DebugManager.unregister(mod)
     MOD_REF = nil
     mod:RemoveCallback(ModCallbacks.MC_POST_ENTITY_KILL, OnKill)
     mod:RemoveCallback(ModCallbacks.MC_PRE_PICKUP_COLLISION, OnPickupCollision)
+    mod:RemoveCallback(ModCallbacks.MC_PRE_PLAYER_COLLISION, OnPlayerCollision)
 end
 
 return DebugManager
