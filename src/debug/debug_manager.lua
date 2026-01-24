6c080f760a97f28e3b0527448cdb6776db9a912d
afournier <afournier@solocal.com>
2026-01-24 16:13:36 +0100

    fix: debug pickup collision

--- src/debug/debug_manager.lua
+++ src/debug/debug_manager.lua
@@ -7,14 +7,20 @@ local function OnKill(_, entity)
     Logger.debug("Entity killed:", "Type =", entity.Type, "Variant =", entity.Variant, "SubType =", entity.SubType)
 end
 
+local function OnPickupCollision(_, entity, _)
+    Logger.debug("Pickup collided:", "Type =", entity.Type, "Variant =", entity.Variant, "SubType =", entity.SubType)
+end
+
 function DebugManager.register(mod)
     MOD_REF = mod
     mod:AddCallback(ModCallbacks.MC_POST_ENTITY_KILL, OnKill)
+    mod:AddCallback(ModCallbacks.MC_PRE_PICKUP_COLLISION, OnPickupCollision)
 end
 
 function DebugManager.unregister(mod)
     MOD_REF = nil
     mod:RemoveCallback(ModCallbacks.MC_POST_ENTITY_KILL, OnKill)
+    mod:RemoveCallback(ModCallbacks.MC_PRE_PICKUP_COLLISION, OnPickupCollision)
 end
 
 return DebugManager
